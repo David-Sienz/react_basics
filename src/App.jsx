af48e34ee754ea9e8c6518ae3176e432a1e17bf3
David-Sienz <david.sienz@outlook.com>
2025-03-31 16:21:52 +0200

    started 03_DemoProject and refactored files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
-import Excercise2 from './components/pages/Excercise2'
+import React from 'react';
+import Excercise_DempProject_Cards_Daten from './components/pages/Excercise_DemoProject_Cards_Daten'
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Excercise2></Excercise2>
+      <Excercise_DempProject_Cards_Daten></Excercise_DempProject_Cards_Daten>
     </>
   )
 }
---
+++ src/components/pages/Excercise_DemoProject_Cards_Daten.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import PeopleContainer from '../PeopleContainer'
+
+export default function Excercise_DemoProject_Cards_Daten() {
+  return (
+    <h1>Excercise_DemoProject_Cards_Daten</h1>
+  )
+}

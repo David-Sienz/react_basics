f00dc1bacfdf08484e65e7e5dffc55d3cdd17f5d
David-Sienz <david.sienz@outlook.com>
2025-04-07 16:23:19 +0200

    worked on 04_demo_filter

--- src/components/PeopleContainer.tsx
+++ src/components/PeopleContainer.tsx
@@ -9,27 +9,44 @@ export default function PeopleContainer() {
   }
 
   const [people, setPeople] = useState<Person[]>([]);
-
+  const [filteredPeople, setFilteredPeople] = useState<Person[]>([]);
   useEffect(() => {
     fetch("https://67c08172b9d02a9f224a0b60.mockapi.io/person").then((res) =>
       res.json().then((data) => {
         setPeople(data);
+        setFilterPeople(filterPeople(people));
       })
     );
   }, []);
 
+  const filterPeople = (filter) => {
+    let filtered = people.filter(person => person.name.includes(filter));
+    return filtered;
+  }
+
   return (
     <div>
-      <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
-        {people.map((person) => {
-          return (
-            <CardPeople
-              name={person.name}
-              image={person.avatar}
-              occupation={person.jobtitle}
-            />
-          );
-        })}
+      <div>
+        <h1>People</h1>
+        <div className="border">
+          <input className="border p-4" placeholder="Search" type="text" onChange={(el)=>{
+            console.log(el.target.value);
+            filterPeople(el.target.value);
+          }}/>
+        </div>
+      </div>  
+      <div>
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
+          {filterPeople.map((person) => {
+            return (
+              <CardPeople
+                name={person.name}
+                image={person.avatar}
+                occupation={person.jobtitle}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
---
+++ src/components/pages/Excercise_demo_filter.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PeopleContainer from '../PeopleContainer'
+
+export default function Excercise_DemoProject_Cards_Daten() {
+  return (
+    <div className="flex flex-col gap-4">
+      <PeopleContainer></PeopleContainer>
+    </div>
+  )
+}
